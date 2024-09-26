d72f21dcc524eea4be64ce9edef87e39ffb6a770
csabagy00 <csaba.gyo00@gmail.com>
2024-09-26 11:53:00 +0200

    Implemented sidebar view

---
+++ MovieTracker/MovieTracker/Views/SideBar.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace MovieTracker.Views;
+
+public partial class SideBar : UserControl
+{
+    public SideBar()
+    {
+        InitializeComponent();
+    }
+}
