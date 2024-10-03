3c2c236ca6d33bcd82945128138198ec83fd7231
csabagy00 <csaba.gyo00@gmail.com>
2024-10-03 18:05:27 +0200

    Changed the type of Genres and ReleaseDate properties

--- MovieTracker/MovieTracker/Models/Item.cs
+++ MovieTracker/MovieTracker/Models/Item.cs
@@ -3,7 +3,7 @@ namespace MovieTracker.Models;
 public abstract class Item
 {
     public string Title { get; set; }
-    public DateOnly ReleaseDate { get; set; }
-    public List<string> Genres { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public List<Genre> Genres { get; set; }
     public bool Watched { get; set; }
 }
