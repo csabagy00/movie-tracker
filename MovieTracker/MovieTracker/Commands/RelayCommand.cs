09810483a5612d1777657759b25d720aca9b3d2c
csabagy00 <csaba.gyo00@gmail.com>
2024-10-03 12:38:26 +0200

    Added default null value to canExecute parameter

--- MovieTracker/MovieTracker/Commands/RelayCommand.cs
+++ MovieTracker/MovieTracker/Commands/RelayCommand.cs
@@ -7,7 +7,7 @@ public class RelayCommand<T> : CommandBase
     private readonly Action<T> _execute;
     private readonly Func<T, bool> _canExecute;
 
-    public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+    public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
     {
         _execute = execute ?? throw new ArgumentNullException(nameof(execute));
         _canExecute = canExecute;
