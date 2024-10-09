12719d6da3088c514fb38d1154485a733954c4ee
csabagy00 <csaba.gyo00@gmail.com>
2024-10-09 22:25:06 +0200

    Added DbContext property & method for adding to the db

--- MovieTracker/MovieTracker/Models/Movie.cs
+++ MovieTracker/MovieTracker/Models/Movie.cs
@@ -1,3 +1,5 @@
+using MovieTracker.Database;
+
 namespace MovieTracker.Models;
 
 public class Movie : Item
@@ -5,17 +7,31 @@ public class Movie : Item
     public int Id { get; set; }
     
     public int Length { get; set; }
+    public MovieTrackerContext Context { get; set; }
 
-    public Movie()
+    public Movie(MovieTrackerContext movieTrackerContext)
     {
+        Genres = new List<Genre>();
+        Context = movieTrackerContext;
     }
     
-    public Movie(string title, DateTime releaseDate, List<Genre> genres, int length, bool watched)
+    public Movie(string title, DateTime releaseDate, List<Genre> genres, int length, bool watched, MovieTrackerContext movieTrackerContext)
     {
         Title = title;
         ReleaseDate = releaseDate;
         Genres = genres;
         Length = length;
         Watched = watched;
+        Context = movieTrackerContext;
+    }
+
+    public void AddMovieToDb(object obj)
+    {
+        if (obj is Movie) 
+        {
+            Movie movie = (Movie)obj;
+            Context.Movies.Add(movie);
+            Context.SaveChanges();
+        }
     }
 }
