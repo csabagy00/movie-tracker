6b5a28855104d5627f69a173397d1bad93d54be3
csabagy00 <csaba.gyo00@gmail.com>
2024-10-09 14:25:37 +0200

    Implemented AddMovieViewModel

---
+++ MovieTracker/MovieTracker/ViewModels/AddMovieViewModel.cs
@@ -0,0 +1,13 @@
+using MovieTracker.Models;
+
+namespace MovieTracker.ViewModels;
+
+public class MovieViewModel
+{
+    private Movie _movie;
+    public Movie Movie
+    {
+        get { return _movie; }
+    }
+    
+}
