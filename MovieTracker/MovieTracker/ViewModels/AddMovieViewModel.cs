2fc801e3670b986b2f15ed6a4c573da799aacfb9
csabagy00 <csaba.gyo00@gmail.com>
2024-10-09 22:26:47 +0200

    Fixed getters and setters of properties

--- MovieTracker/MovieTracker/ViewModels/AddMovieViewModel.cs
+++ MovieTracker/MovieTracker/ViewModels/AddMovieViewModel.cs
@@ -1,34 +1,48 @@
+using MovieTracker.Commands;
+using MovieTracker.Database;
 using MovieTracker.Models;
+using System.Linq;
+using System.Windows.Input;
 
 namespace MovieTracker.ViewModels;
 
 public class AddMovieViewModel : ViewModelBase
 {
-    private string _title;
+    public ICommand AddMovieCommand { get; set; }
+    private Movie Movie { get; set; }
+
+    public AddMovieViewModel(MovieTrackerContext movieTrackerContext)
+    {
+        Movie = new Movie(movieTrackerContext);
+        AddMovieCommand = new RelayCommand<object>(ExecuteAddMovieCommand);
+    }
+
+    public void ExecuteAddMovieCommand(object parameter)
+    {
+        Movie.AddMovieToDb(Movie);
+    }
+
     public string Title
     {
-        get { return _title; }
+        get { return Movie.Title; }
         set
         {
-            if (_title != value)
+            if (Title != value)
             {
-                _title = value;
-                Console.WriteLine(_title);
+                Movie.Title = value;
                 OnPropertyChanged(nameof(Title));
             }
         }
     }
 
-    private DateTime _releaseDate;
     public DateTime ReleaseDate
     {
-        get { return _releaseDate; }
+        get { return Movie.ReleaseDate; }
         set
         {
-            if (_releaseDate != value)
+            if (ReleaseDate != value)
             {
-                _releaseDate = value;
-                Console.WriteLine(_releaseDate);
+                Movie.ReleaseDate = value;
                 OnPropertyChanged(nameof(ReleaseDate));
             }
         }
@@ -40,10 +54,27 @@ public class AddMovieViewModel : ViewModelBase
         get { return _genreOne; }
         set
         {
+            if(_genreOne is not null)
+            {
+                Genre? selected = Movie.Genres.FirstOrDefault(gen => gen.Name == _genreOne);
+
+                if (selected != null && Movie.Genres.Contains(selected))
+                {
+                    Movie.Genres.Remove(selected);
+                }
+            }
+            
+
             if (GenreOne != value)
             {
                 _genreOne = value;
                 OnPropertyChanged(nameof(GenreOne));
+
+                if(GenreOne != null)
+                {
+                    Genre genre = new Genre(_genreOne);
+                    Movie.Genres.Add(genre);
+                }
             }
         }
     }
@@ -51,13 +82,29 @@ public class AddMovieViewModel : ViewModelBase
     private string? _genreTwo;
     public string GenreTwo
     {
-        get { return _genreTwo; }
+        get { return _genreTwo == null ? "" : _genreTwo; }
         set
         {
+            if(_genreTwo is not null)
+            {
+                Genre? selected = Movie.Genres.FirstOrDefault(gen => gen.Name == _genreTwo);
+
+                if(selected != null && Movie.Genres.Contains(selected))
+                {
+                    Movie.Genres.Remove(selected);
+                }
+            }
+
             if (GenreTwo != value)
             {
                 _genreTwo = value;
                 OnPropertyChanged(nameof(GenreTwo));
+
+                if(GenreTwo != null)
+                {
+                    Genre genre = new Genre(_genreTwo);
+                    Movie.Genres.Add(genre);
+                }
             }
         }
     }
@@ -65,40 +112,54 @@ public class AddMovieViewModel : ViewModelBase
     private string? _genreThree;
     public string GenreThree
     {
-        get { return _genreThree; }
+        get { return _genreThree == null ? "" : _genreThree; }
         set
         {
+            if(_genreThree is not null)
+            {
+                Genre? selected = Movie.Genres.FirstOrDefault(gen => gen.Name == _genreThree);
+
+                if(selected != null && Movie.Genres.Contains(selected))
+                {
+                    Movie.Genres.Remove(selected);
+                }
+            }
+
             if (GenreThree != value)
             {
                 _genreThree = value;
                 OnPropertyChanged(nameof(GenreThree));
+
+                if(GenreThree != null)
+                {
+                    Genre genre = new Genre(_genreThree);
+                    Movie.Genres.Add(genre);
+                }
             }
         }
     }
 
-    private int _length;
     public int Length
     {
-        get { return _length; }
+        get { return Movie.Length; }
         set
         {
             if (Length != value)
             {
-                _length = value;
+                Movie.Length = value;
                 OnPropertyChanged(nameof(Length));
             }
         }
     }
 
-    private bool _watched;
     public bool Watched
     {
-        get { return _watched; }
+        get { return Movie.Watched; }
         set
         {
             if (Watched != value)
             {
-                _watched = value;
+                Movie.Watched = value;
                 OnPropertyChanged(nameof(Watched));
             }
         }
