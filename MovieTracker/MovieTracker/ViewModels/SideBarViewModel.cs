c612bc4ede0213a676ade0da7ca7c85418be5e0c
Csaba <csaba.gyo00@gmail.com>
2024-10-12 12:51:35 +0200

    Created command for showing Movies

--- MovieTracker/MovieTracker/ViewModels/SideBarViewModel.cs
+++ MovieTracker/MovieTracker/ViewModels/SideBarViewModel.cs
@@ -10,18 +10,25 @@ public class SideBarViewModel : ViewModelBase
     private AddItemViewModel AddItemViewModel { get; set; }
     private MoviesViewModel MoviesViewModel { get; set; }
     public ICommand ShowAddItemCommand { get; set; }
+    public ICommand ShowMoviesCommand { get; set; }
 
     public SideBarViewModel(MainViewModel mainViewModel, MovieTrackerContext movieTrackerContext)
     {
         MainViewModel = mainViewModel;
         ShowAddItemCommand = new RelayCommand<object>(ShowAddItemView);
+        ShowMoviesCommand = new RelayCommand<object>(ShowMoviesView);
         AddItemViewModel = new AddItemViewModel(movieTrackerContext);
         MoviesViewModel = new MoviesViewModel(movieTrackerContext);
     }
 
     private void ShowAddItemView(object obj)
     {
-        
+
         MainViewModel.SelectedView = AddItemViewModel;
+    }
+
+    private void ShowMoviesView(object obj) 
+    {
+        MainViewModel.SelectedView = MoviesViewModel;
     }
 }
