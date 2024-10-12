0a095d2d26eddc1316bc4050ec5022ff68f2175b
Csaba <csaba.gyo00@gmail.com>
2024-10-12 12:52:16 +0200

    Created MoviesViewModel

---
+++ MovieTracker/MovieTracker/ViewModels/MoviesViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTracker.Database;
+using MovieTracker.Models;
+using System.Collections.ObjectModel;
+
+namespace MovieTracker.ViewModels;
+
+public class MoviesViewModel : ViewModelBase
+{
+    private ObservableCollection<Movie> _movies;
+    public ObservableCollection<Movie> Movies 
+    { 
+        get { return _movies; } 
+        set {
+                _movies = value;
+                OnPropertyChanged(nameof(Movies));
+            } 
+    }
+    public MovieTrackerContext Context { get; set; }
+
+    public MoviesViewModel(MovieTrackerContext context)
+    {
+        Context = context;
+        GetWatchedMovies();
+
+    }
+
+    private void GetWatchedMovies()
+    {
+        Movies = new ObservableCollection<Movie>(Context.Movies.Where(m => m.Watched == true).Include(m => m.Genres).ToList());
+    }
+}
+
