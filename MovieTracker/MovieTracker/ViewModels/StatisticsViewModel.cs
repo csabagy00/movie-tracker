330f9c59c61f1fc9173329bdebff1e3edbb2c348
csabagy00 <csaba.gyo00@gmail.com>
2024-10-24 16:36:08 +0200

    Implemented Statistics viewmodel

---
+++ MovieTracker/MovieTracker/ViewModels/StatisticsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTracker.ViewModels;
+
+public class StatisticsViewModel : ViewModelBase
+{
+    public ObservableCollection<string> Types { get; set; }
+
+    private string? _selectedType;
+
+    public string SelectedType
+    {
+        get { return _selectedType; }
+        set
+        {
+            if (value != SelectedType)
+            {
+                _selectedType = value;
+                OnPropertyChanged(nameof(SelectedType));
+            }
+        }
+    }
+
+    public StatisticsViewModel()
+    {
+        Types = new ObservableCollection<string>{"All", "Movies", "Series"};
+    }
+}
