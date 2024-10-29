5db48a113626202698a5146b639180520cac5344
csabagy00 <csaba.gyo00@gmail.com>
2024-10-29 15:12:09 +0100

    Created Model for logic

---
+++ MovieTracker/MovieTracker/Models/MovieStatsModel.cs
@@ -0,0 +1,25 @@
+﻿using MovieTracker.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+
+namespace MovieTracker.Models;
+
+public class MovieStatsModel
+{
+    private MovieTrackerContext _context;
+
+    public MovieStatsModel(MovieTrackerContext context)
+    {
+        _context = context;
+    }
+
+    public int GetWatchedMoviesCount()
+    {
+        return _context.Movies.Where(m => m.Watched == true).Count();
+    }
+}
+
