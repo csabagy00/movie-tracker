ddc0a73f289ba03c1efd092f00b8c4d6c5d56a58
csabagy00 <csaba.gyo00@gmail.com>
2024-10-29 16:12:43 +0100

    Implemented methods with logic for viewmodel's properties

--- MovieTracker/MovieTracker/Models/MovieStatsModel.cs
+++ MovieTracker/MovieTracker/Models/MovieStatsModel.cs
@@ -19,7 +19,69 @@ public class MovieStatsModel
 
     public int GetWatchedMoviesCount()
     {
-        return _context.Movies.Where(m => m.Watched == true).Count();
+        return _context.Movies
+            .Where(m => m.Watched == true)
+            .Count();
+    }
+
+    public int GetUnwatchedMoviesCount()
+    {
+        return _context.Movies
+            .Where(m => m.Watched == false)
+            .Count();
+    }
+
+    public int GetTimeSpent()
+    {
+        return _context.Movies
+            .Where(m => m.Watched == true)
+            .Select(m => m.Length)
+            .Sum();
+    }
+
+    public string GetLongestMovieTitle()
+    {
+        int max;
+
+        max = _context.Movies
+            .Where(m => m.Watched == true)
+            .Select(m => m.Length)
+            .Max();
+
+        return _context.Movies
+            .Where(m => m.Watched == true && m.Length == max)
+            .First().Title; ;
+    }
+
+    public string GetShortestMovieTitle()
+    {
+        int min;
+
+        min = _context.Movies
+            .Where(m => m.Watched == true)
+            .Select(m => m.Length)
+            .Sum();
+
+        return _context.Movies
+            .Where(m => m.Watched == true && m.Length == min)
+            .First().Title;
+    }
+
+    public string GetMostWatchedGenre()
+    {
+        return _context.Genres
+            .GroupBy(g => g.Name)
+            .OrderByDescending(g => g.Count())
+            .Select(g => g.First())
+            .First().Name;
+    }
+
+    public float GetCompletionRate()
+    {
+        float allMovies = _context.Movies.Count();
+        float watched = _context.Movies.Where(m => m.Watched == true).Count();
+
+        return allMovies / watched * 100;
     }
 }
 
