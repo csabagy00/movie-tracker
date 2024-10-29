38551bf87e8f233b595d5dbbfe2328ba6b7fc3bc
csabagy00 <csaba.gyo00@gmail.com>
2024-10-29 15:11:45 +0100

    Implemented viewmodel with properties fvro MovieStats

---
+++ MovieTracker/MovieTracker/ViewModels/MovieStatsViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieTracker.ViewModels;
+
+public class MovieStatsViewModel : ViewModelBase
+{
+    private int _moviesCount;
+    public int MoviesCount
+    {
+        get { return _moviesCount; }
+        set
+        {
+
+        }
+    }
+
+    private int _moviesCountUnwatched;
+    public int MoviesCountUnwatched
+    {
+        get { return _moviesCountUnwatched; }
+        set
+        {
+
+        }
+    }
+
+    private int _timeSpent;
+    public int TimeSpent
+    {
+        get { return _timeSpent; }
+        set
+        {
+
+        }
+    }
+
+    private string _longest;
+    public string Longest
+    {
+        get { return _longest; }
+        set
+        {
+
+        }
+    }
+
+    private string _shortest;
+    public string Shortest
+    {
+        get { return _shortest; }
+        set
+        {
+
+        }
+    }
+
+    private string _mostWatchedGenre;
+    public string MostWatched
+    {
+        get { return _mostWatchedGenre; }
+        set
+        {
+
+        }
+    }
+
+    private float _completionRate;
+    public float CompletionRate
+    {
+        get { return _completionRate; }
+        set
+        {
+
+        }
+    }
+}
+
